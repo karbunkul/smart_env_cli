8acebd7459af9cc5bbe3b44593cd5dbc219b5cb0
apohodun <apohodun@sdvor.com>
2023-10-14 13:25:37 +0500

    Import for CastType

--- lib/src/config/cast_type.dart
+++ lib/src/config/cast_type.dart
@@ -9,4 +9,14 @@ enum CastType {
   final String type;
 
   const CastType(this.type);
+
+  CastType import(String type) {
+    return CastType.values.firstWhere(
+      (element) => element.type == type,
+      orElse: () {
+        final availableValues = CastType.values.map((e) => e.type).join(', ');
+        throw ArgumentError('Available values: $availableValues');
+      },
+    );
+  }
 }
